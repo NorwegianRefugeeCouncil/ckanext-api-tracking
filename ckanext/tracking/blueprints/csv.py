5c8a00bcf4cbe34b8870ca573ee9b8b6501b9bb6
avdata99 <andres@data99.com.ar>
2024-10-30 11:57:14 -0300

    refactor a little bit

--- ckanext/tracking/blueprints/csv.py
+++ ckanext/tracking/blueprints/csv.py
@@ -3,14 +3,13 @@ import logging
 from io import StringIO
 
 from flask import Blueprint, Response
-from ckan import model
 from ckan.common import current_user
 from ckan.plugins import toolkit
 
-from ckanext.tracking.queries.api import (
-    get_all_token_usage,
-    get_most_accessed_token,
-    get_most_accessed_dataset_with_token,
+from ckanext.tracking.queries.data import (
+    all_token_usage_data,
+    most_accessed_token_data,
+    most_accessed_dataset_with_token_data,
 )
 
 
@@ -25,33 +24,17 @@ def most_accessed_dataset_with_token_csv():
     current_user_name = current_user.name if current_user else None
     context = {'user': current_user_name}
     toolkit.check_access('most_accessed_dataset_with_token_csv', context)
-    data = get_most_accessed_dataset_with_token(limit=10)
-
+    data = most_accessed_dataset_with_token_data(limit=10)
+    # If no rows, return empty 204 CSV
+    if len(data) == 0:
+        return Response('', status=204)
     # Create CSV including package details
-    rows = []
-    for row in data:
-        object_id = row['object_id']
-        obj = model.Package.get(object_id)
-        if obj:
-            obj_title = obj.title
-            object_url = toolkit.url_for('dataset.read', id=obj.id, qualified=True)
-        else:
-            obj_title = None
-            object_url = None
-
-        rows.append({
-            'Dataset ID': object_id,
-            'Dataset title': obj_title,
-            'Dataset url': object_url,
-            'total': row['total'],
-        })
-
-    headers = ['Dataset ID', 'Dataset title', 'Dataset url', 'total']
+    headers = data[0].keys()
     buffer = StringIO()
     writer = csv.DictWriter(buffer, fieldnames=headers)
 
     writer.writeheader()
-    for row in rows:
+    for row in data:
         writer.writerow(row)
 
     response = Response(buffer.getvalue(), mimetype='text/csv')
@@ -67,35 +50,16 @@ def most_accessed_token_csv():
     current_user_name = current_user.name if current_user else None
     context = {'user': current_user_name}
     toolkit.check_access('most_accessed_token_csv', context)
-    data = get_most_accessed_token(limit=10)
-    # Create CSV including package details
-    rows = []
-    for row in data:
-        user_id = row['user_id']
-        user = model.User.get(user_id)
-        if user:
-            user_title = user.fullname
-            user_name = user.name
-            user_url = toolkit.url_for('user.read', id=user.name, qualified=True)
-        else:
-            user_title = None
-            user_name = None
-            user_url = None
-
-        rows.append({
-            'User ID': user_id,
-            'User fullname': user_title,
-            'User name': user_name,
-            'User url': user_url,
-            'total': row['total'],
-        })
-
-    headers = ['User ID', 'User fullname', 'User name', 'User url', 'total']
+    data = most_accessed_token_data(limit=10)
+    # If no rows, return empty 204 CSV
+    if len(data) == 0:
+        return Response('', status=204)
+    headers = data[0].keys()
     buffer = StringIO()
     writer = csv.DictWriter(buffer, fieldnames=headers)
 
     writer.writeheader()
-    for row in rows:
+    for row in data:
         writer.writerow(row)
 
     response = Response(buffer.getvalue(), mimetype='text/csv')
@@ -111,34 +75,18 @@ def all_token_usage_csv():
     current_user_name = current_user.name if current_user else None
     context = {'user': current_user_name}
     toolkit.check_access('most_accessed_token_csv', context)
-    data = get_all_token_usage(limit=1000)
+    data = all_token_usage_data(limit=1000)
+    # If no rows, return empty 204 CSV
+    if len(data) == 0:
+        return Response('', status=204)
+
     # Create CSV including package details
-    rows = []
-    for row in data:
-        user_id = row['user_id']
-        user = model.User.get(user_id)
-        user_name = user.name if user else None
-
-        rows.append({
-            'User ID': user_id,
-            'User name': user_name,
-            'Token name': row['token_name'],
-            'Tracking type': row['tracking_type'],
-            'Tracking sub type': row['tracking_sub_type'],
-            'Object type': row['object_type'],
-            'Object ID': row['object_id'],
-            'Timestamp': row['timestamp'],
-        })
-
-    headers = [
-        'User ID', 'User name', 'Token name', 'Tracking type', 'Tracking sub type',
-        'Object type', 'Object ID', 'Timestamp'
-    ]
+    headers = data[0].keys()
     buffer = StringIO()
     writer = csv.DictWriter(buffer, fieldnames=headers)
 
     writer.writeheader()
-    for row in rows:
+    for row in data:
         writer.writerow(row)
 
     response = Response(buffer.getvalue(), mimetype='text/csv')
---
+++ ckanext/tracking/queries/data/all.py
@@ -0,0 +1,70 @@
+"""
+Post-preocessed data after the DB queries and before the CSV generation
+"""
+
+import logging
+from ckan import model
+from ckan.plugins import toolkit
+
+from ckanext.tracking.queries.api import get_all_token_usage
+
+
+log = logging.getLogger(__name__)
+
+
+def all_token_usage_data(limit=1000):
+    """ Get all tokens usage """
+
+    data = get_all_token_usage(limit=limit)
+    # Create CSV including package details
+    rows = []
+
+    for row in data:
+        user_id = row['user_id']
+        user = model.User.get(user_id)
+        if user:
+            user_name = user.fullname or user.name
+        else:
+            user_name = None
+        object_id = row['object_id']
+        object_type = row['object_type']
+        organization_url = None
+        organization_title = None
+        obj = None
+        obj_title = None
+        object_url = None
+        if object_id:
+            if object_type == 'dataset':
+                obj = model.Package.get(object_id)
+                obj_title = obj.title
+                object_url = toolkit.url_for('dataset.read', id=obj.id)
+                pkg = toolkit.get_action('package_show')({'ignore_auth': True}, {'id': obj.id})
+                owner_org = pkg.get('organization', {})
+                organization_title = owner_org.get('title')
+                organization_url = toolkit.url_for('organization.read', id=owner_org.get('id'))
+            elif object_type == 'resource':
+                obj = model.Resource.get(object_id)
+                obj_title = obj.name
+                object_url = toolkit.url_for('dataset_resource.read', id=obj.package_id, resource_id=obj.id)
+            elif object_type == 'organization':
+                obj = model.Organization.get(object_id)
+                obj_title = obj.title
+                object_url = toolkit.url_for('organization.read', id=obj.id)
+
+        rows.append({
+            'timestamp': row['timestamp'],
+            'user_id': user_id,
+            'user_name': user_name,
+            'user_fullname': user.fullname if user else None,
+            'token_name': row['token_name'],
+            'tracking_type': row['tracking_type'],
+            'tracking_sub_type': row['tracking_sub_type'],
+            'object_type': object_type,
+            'object_id': object_id,
+            'object_title': obj_title,
+            'object_url': object_url,
+            'organization_url': organization_url,
+            'organization_title': organization_title,
+        })
+
+    return rows
