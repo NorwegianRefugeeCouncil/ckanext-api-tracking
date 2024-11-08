653ffd43ba9085d3934a9e23bcc5b7144f4b239b
avdata99 <andres@data99.com.ar>
2024-11-08 15:05:32 -0300

    re-fix test

--- ckanext/api_tracking/tests/test_plugin.py
+++ ckanext/api_tracking/tests/test_plugin.py
@@ -51,7 +51,7 @@ import pytest
 from ckan import plugins
 
 
-@pytest.mark.ckan_config("ckan.plugins", "tracking")
+@pytest.mark.ckan_config("ckan.plugins", "api_tracking")
 @pytest.mark.usefixtures("with_plugins")
 def test_plugin():
     assert plugins.plugin_loaded("api_tracking")
