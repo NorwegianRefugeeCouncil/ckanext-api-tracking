65d661ddc2c8be62f88e20501261d98701639b13
avdata99 <andres@data99.com.ar>
2024-11-11 14:18:10 -0300

    undo hotfix

--- ckanext/api_tracking/migration/api_tracking/versions/001_95aed1f25344_init_extension_models.py
+++ ckanext/api_tracking/migration/api_tracking/versions/001_95aed1f25344_init_extension_models.py
@@ -6,8 +6,8 @@ Create Date: 2024-09-13 11:31:04.000000
 
 """
 from alembic import op
-# import sqlalchemy as sa
-# from sqlalchemy.dialects import postgresql
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -18,19 +18,18 @@ depends_on = None
 
 
 def upgrade():
-    # op.create_table(
-    #     "tracking_usage",
-    #     sa.Column("id", sa.UnicodeText, primary_key=True),
-    #     sa.Column("timestamp", sa.DateTime, server_default=sa.func.current_timestamp()),
-    #     sa.Column("user_id", sa.UnicodeText, nullable=True),
-    #     sa.Column("tracking_type", sa.UnicodeText, nullable=True),
-    #     sa.Column("tracking_sub_type", sa.UnicodeText, nullable=True),
-    #     sa.Column("extras", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
-    #     sa.Column("token_name", sa.UnicodeText, nullable=True),
-    #     sa.Column("object_id", sa.UnicodeText, nullable=True),
-    #     sa.Column("object_type", sa.UnicodeText, nullable=True),
-    # )
-    pass
+    op.create_table(
+        "tracking_usage",
+        sa.Column("id", sa.UnicodeText, primary_key=True),
+        sa.Column("timestamp", sa.DateTime, server_default=sa.func.current_timestamp()),
+        sa.Column("user_id", sa.UnicodeText, nullable=True),
+        sa.Column("tracking_type", sa.UnicodeText, nullable=True),
+        sa.Column("tracking_sub_type", sa.UnicodeText, nullable=True),
+        sa.Column("extras", postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column("token_name", sa.UnicodeText, nullable=True),
+        sa.Column("object_id", sa.UnicodeText, nullable=True),
+        sa.Column("object_type", sa.UnicodeText, nullable=True),
+    )
 
 
 def downgrade():
