9af866460eb4c8b5628c23b89c700fe7a63e9a24
avdata99 <andres@data99.com.ar>
2024-12-06 12:53:06 -0300

    Splitting resource and dataset stats

--- ckanext/api_tracking/queries/data/__init__.py
+++ ckanext/api_tracking/queries/data/__init__.py
@@ -2,4 +2,5 @@
 
 from ckanext.api_tracking.queries.data.all import all_token_usage_data
 from ckanext.api_tracking.queries.data.dataset import most_accessed_dataset_with_token_data
+from ckanext.api_tracking.queries.data.resource import most_accessed_resource_with_token_data
 from ckanext.api_tracking.queries.data.token import most_accessed_token_data
