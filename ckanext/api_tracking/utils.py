056fcec1c56ad16d9338abe9b4df68d928254ab4
avdata99 <andres@data99.com.ar>
2025-03-04 11:25:44 -0300

    fix params

--- ckanext/api_tracking/utils.py
+++ ckanext/api_tracking/utils.py
@@ -1,12 +1,19 @@
-def track_logged_in():
+
+def track_logged_in(sender, user):
     """ This function is executed everytime CKAN core
         sends the logged_in signal.
+        This signals comes from Flask-Login.
+
+        Sender <CKANFlask 'ckan.config.middleware.flask_app'>
+        User <User id=c3987980 ...> logged in
+
     """
     pass
 
 
-def track_logged_out():
+def track_logged_out(sender, user):
     """ This function is executed everytime CKAN core
         sends the logged_out signal.
+        This signals comes from Flask-Login.
     """
     pass
